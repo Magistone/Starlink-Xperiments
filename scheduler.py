cbc2571c048875611f8c0c662cbd33cda0cf0098
Magistone <56304476+Magistone@users.noreply.github.com>
2024-05-13 12:38:49 +0200

    Change layout

---
+++ scheduler.py
@@ -0,0 +1,28 @@
+#How often
+#how long (or forever)
+#schedule (start when)
+
+
+def write_data(data):
+    #MongoDB writer
+    pass
+
+def delay():
+    #delay starting scheduled tasks
+    pass
+
+def create():
+    #import module and create task
+    pass
+
+def run():
+    #run task (often/long + forever)
+
+    data = ':)'## running
+    write_data(data)
+    pass
+
+def schedule(config):
+    job = create()
+    delay()
+    run()
